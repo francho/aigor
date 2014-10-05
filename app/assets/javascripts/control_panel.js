8d734bf544dcaa80ddbb4ccc945856cbbe2d934b
Francho Joven <lab@francho.org>
2014-10-06 00:34:33 +0200

    Add RGB slide selectors to control panel

---
+++ app/assets/javascripts/control_panel.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(function() {
+
+  function slide2hex(slideSelector) {
+    var dec = parseInt($(slideSelector).attr('data-slider') * 255 / 100);
+    var hex = dec.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  }
+
+  function getSelectedRgbColor() {
+    return '#' + slide2hex('#slide-red') + slide2hex('#slide-green') + slide2hex('#slide-blue');
+  }
+
+  function colorizeLedsSwitch() {
+    var ledsOn = $('#leds-switch').is(':checked');
+    var rgb = ledsOn ? getSelectedRgbColor() : '';
+
+    $('#leds-switch-label').css({'background': rgb});
+  }
+
+  function switchLeds(turnOn) {
+    colorizeLedsSwitch();
+  }
+
+  $(document).ready(function() {
+    $('#slide-red').on('change.fndtn.slider', colorizeLedsSwitch);
+    $('#slide-green').on('change.fndtn.slider', colorizeLedsSwitch);
+    $('#slide-blue').on('change.fndtn.slider', colorizeLedsSwitch);
+
+    $('#leds-switch').on('click', function() {
+      switchLeds($(this).checked);
+    });
+  });
+
+})();
