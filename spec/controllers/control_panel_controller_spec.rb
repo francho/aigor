2764d3d27eb4898e24f707bd993d66ab1b9f7772
Francho Joven <lab@francho.org>
2014-10-08 09:31:24 +0200

    Move test to rspec

---
+++ spec/controllers/control_panel_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe ControlPanelController do
+
+  describe 'home' do
+    before do
+      get :index
+    end
+
+    it 'exists' do
+      expect(response).to be_success
+    end
+
+    it 'is html' do
+      expect(response.headers['Content-Type']).to include 'text/html; charset=utf-8'
+    end
+  end
+
+  describe 'webservices' do
+
+    describe 'switch_led' do
+      before do
+        allow(ApplicationHelper).to receive(:websocket_broadcast)
+        post :switch_led, format: :json, power_on: true, color: [ red: 'ff', green: 'aa', blue: 'ff' ]
+      end
+
+      it 'accept POST' do
+        expect(response).to be_success
+      end
+
+      it 'is json' do
+        expect(response.headers['Content-Type']).to include 'application/json; charset=utf-8'
+      end
+
+      it 'emit a websocket message' do
+        expect(ApplicationHelper).to have_received(:websocket_broadcast)
+      end
+    end
+
+  end
+
+end
---
+++ spec/helpers/control_panel_helper_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe ControlPanelHelper do
+end
