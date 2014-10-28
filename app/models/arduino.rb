4e8df34ec2812c470d1a5d152998979ad12181b9
Francho Joven <lab@francho.org>
2014-10-28 23:55:45 +0100

    Move arduino config to aigor.yml

--- app/models/arduino.rb
+++ app/models/arduino.rb
@@ -2,11 +2,12 @@ class Arduino
   include Singleton
 
   def initialize
-    port_str  = "/dev/cu.usbmodem1411" #may be different for you
-    baud_rate = 115200
-    data_bits = 8
-    stop_bits = 1
-    parity    = SerialPort::NONE
+    port_str  = Rails.application.config.aigor.arduino['port']
+    baud_rate = Rails.application.config.aigor.arduino['baud_rate'].to_i
+    data_bits = Rails.application.config.aigor.arduino['data_bits'].to_i
+    stop_bits = Rails.application.config.aigor.arduino['stop_bits'].to_i
+    parity    = Rails.application.config.aigor.arduino['parity'].to_i
+
 
     @serial_port = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
     @serial_port.read_timeout = 100
