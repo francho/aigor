2b6af88f77f5543580eb3e031f0506179477f1df
Francho Joven <lab@francho.org>
2014-10-29 23:01:06 +0100

    Tweak capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,43 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'aigor'
+set :repo_url, 'https://github.com/francho/aigor.git'
+set :branch, 'master'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+# Capistrano-rails gem
+set :rails_env, 'production'
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+# Capistrano-rbenv gem
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.1'
+
+# Capistrano-bundler gem
+set :bundle_without, %w{development test darwin}.join(' ')
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/aigor.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
@@ -40,7 +45,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,10 +4,11 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{pi@192.168.33.3}
+role :web, %w{pi@192.168.33.3}
+role :db,  %w{pi@192.168.33.3}
 
+set :deploy_to, '/home/pi/aigor'
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +16,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '192.168.33.3', user: 'pi', roles: %w{web app}
 
 
 # Custom SSH Options
