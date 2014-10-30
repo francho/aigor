51efdb22365257baf78f1839c2db578546912019
Francho Joven <lab@francho.org>
2014-10-30 17:43:20 +0100

    Tweaks in deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/aigor.yml}
+set :linked_files, %w{config/aigor.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -52,11 +52,12 @@ namespace :deploy do
   after :publishing, :restart
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    on roles(:app), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+         execute :rake, 'cache:clear'
+         queue %[kill -s SIGUSR1 $(cat tmp/pids/server.pid)]
+      end
     end
   end
 
