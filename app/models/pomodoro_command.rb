be92835af258676d0d4d3f8bbd3e43dc28c2714e
Francho Joven <lab@francho.org>
2014-11-11 01:20:21 +0100

    PomodoroCommand refactor

--- app/models/pomodoro_command.rb
+++ app/models/pomodoro_command.rb
@@ -29,45 +29,45 @@ class PomodoroCommand
   end
 
   def pomodoro_job
-    scheduler.jobs(tags: 'pomodoro').first
+    scheduler.jobs(:tags => 'pomodoro').first
   end
 
   def color_countdown_job
-    scheduler.jobs(tags: 'countdown').first
+    scheduler.jobs(:tags => 'countdown').first
   end
 
   private
 
   def schedule_end(minutes)
     ColorizeLedsCommand.new(255, 0, 0).execute!
-    @pomodoro_job = @scheduler.schedule_in("#{minutes}m", tag: 'pomodoro') do
-    @color_command.set_color(0, 255, 0)
-    @color_command.execute!
-    @scheduler.unschedule color_countdown_job
+    @pomodoro_job = @scheduler.schedule_in("#{minutes}m", :tag => 'pomodoro') do
+      @color_command.set_color(0, 255, 0)
+      @color_command.execute!
+      @scheduler.unschedule color_countdown_job
+    end
   end
-end
 
-def start_color_countdown
-  @green_step= 255 / (pomodoro_job.time - Time.now)
+  def start_color_countdown
+    @green_step= 255 / (pomodoro_job.time - Time.now)
 
-  @color_countdown_job = @scheduler.every('1s', tag: 'countdown') do
-  colorize_step
-end
+    @color_countdown_job = @scheduler.every('1s', :tag => 'countdown') do
+      colorize_step
+    end
 
-end
+  end
 
-def colorize_step
-  return unless pomodoro_job.scheduled? && pomodoro_job.time
-  remain = pomodoro_job.time - Time.now
-  green  = 255 - (@green_step * remain).to_i
-  puts "#{remain} g: #{green} step: #{@green_step}"
+  def colorize_step
+    return unless pomodoro_job.scheduled? && pomodoro_job.time
 
-  return unless (0..255).include? green
+    remain = pomodoro_job.time - Time.now
+    green  = 255 - (@green_step * remain).to_i
 
-  @color_command.set_color(255, green, 0)
-  @color_command.execute!
+    return unless (0..255).include? green
 
-  broadcast(:pomodoro_step, remain)
-end
+    @color_command.set_color(255, green, 0)
+    @color_command.execute!
+
+    broadcast(:notify_pomodoro_step, remain)
+  end
 
 end
